d523030340b35907985d1de016bf45cddb3baad2
RyaNGosling2 <prib-211_582519@volsu.ru>
2022-12-22 18:18:50 +0300

    1.2

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <locale> 
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include "std.h"
+using namespace std;
+
+void main()
+{ 
+	int a,b;
+	cout <<"Введите а -" <<
+	cin >> a;
+	cout <<"Введите b -" <<
+	cin >> b;
+}	
